ea13d1797661b0be2b87bc4842e5af115eed8e25
soufianeroguiai <soufianeroguiai@gmail.com>
2026-06-17 02:54:06 +0100

    update

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,10 +1,3 @@
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
-
 buildscript {
     repositories {
         google()
@@ -12,6 +5,24 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
     }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+// 🔥 FORCE Kotlin 2.0 everywhere (حل مشكل 2.2)
+configurations.all {
+    resolutionStrategy {
+        force("org.jetbrains.kotlin:kotlin-stdlib:2.0.0")
+    }
+}
+
+tasks.register<Delete>("clean") {
+    delete(rootProject.buildDir)
 }
