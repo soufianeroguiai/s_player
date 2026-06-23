81b95d3b20e7313a0ff8c4dba725d075b0866989
soufianeroguiai <soufianeroguiai@gmail.com>
2026-06-23 01:45:42 +0100

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("com.android.application")
-    // id("org.jetbrains.kotlin.android")  // تمت إزالته: Flutter يدعم Kotlin المدمج
+    id("org.jetbrains.kotlin.android")
     id("dev.flutter.flutter-gradle-plugin")
 }
 
